b3afe56af7ec5d03df818ee51291ef20e8da9d79
MonkNL <info@thomasvv.nl>
2024-03-10 10:03:19 +0100

    Update class.page.php

--- class.page.php
+++ class.page.php
@@ -1,18 +1,18 @@
 <?php
 Class page{
 	public 	$pageTitle, 
-					$menuTitle, 
-					$capability, 
-					$pageSlug = '',
-					$callback = null,
-					$priority = 10,
-					$inMenu = false,
-					$subPages = [],
-					$parentSlug = null,
-					$regexVariables = [],
-					$childPages = [],
-					$regex='',
-					$parent = null;	 
+		$menuTitle, 
+		$capability, 
+		$pageSlug = '',
+		$callback = null,
+		$priority = 10,
+		$inMenu = false,
+		$subPages = [],
+		$parentSlug = null,
+		$regexVariables = [],
+		$childPages = [],
+		$regex='',
+		$parent = null;	 
 	
 	public function __construct(string $pageTitle, string $menuTitle, array|string $capability, string $pageSlug, callable $callback = null, int|float $priority= null,bool $inMenu = false,$parentSlug = null) {
 		$this->pageTitle	= $pageTitle; 
@@ -21,18 +21,16 @@ Class page{
 		$this->pageSlug		= $pageSlug;
 		$this->callback		= $callback; 
 		$this->priority		= $priority;
-		$this->inMenu			= $inMenu;
-		$this->parentSlug = $parentSlug;
+		$this->inMenu		= $inMenu;
+		$this->parentSlug 	= $parentSlug;
 		$this->subPages		= [];
-		$slug 						= (substr($pageSlug,-1)=='/'?substr($pageSlug,0,-1):$pageSlug);
-		$this->regex			= '/^'.str_replace('/','\/',$slug).'\/?$/'; // ^ = start, $ = end, \/? = 0 or 1 /
+		$slug 			= (substr($pageSlug,-1)=='/'?substr($pageSlug,0,-1):$pageSlug);
+		$this->regex		= '/^'.str_replace('/','\/',$slug).'\/?$/'; // ^ = start, $ = end, \/? = 0 or 1 /
 	}
 	public function setRegexvariables($matches){
-	
 		$this->regexVariables = $matches;
 	}
 	public function regexSprintF(){
-
 		return preg_replace('/(\(.*\))/U', '%s', $this->pageSlug);
 	}
 	public function addSubPage(page $page){
