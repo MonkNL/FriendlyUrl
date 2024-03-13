dc8742aeff8f10196adb8fe13c19b24046be03d6
MonkNL <info@thomasvv.nl>
2024-03-13 13:23:40 +0100

    Update class.hooks.php

--- classes/class.hooks.php
+++ classes/class.hooks.php
@@ -1,7 +1,17 @@
 <?php
+class Hooks {
+    private static $instance = null;
 
-Class Hooks{
+    private function __construct() {
+    }
 
+
+    public static function getInstance() {
+        if (self::$instance == null) {
+            self::$instance = new Self();
+        }
+        return self::$instance;
+    }
 }
 
 ?>
